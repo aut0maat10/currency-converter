7d43ee784c6e9da667f89dda389e93ceca085294
Robin Pahlman <robin.pahlman@gmail.com>
2018-09-04 21:56:15 -0400

    add CurrencyConverter component

---
+++ src/CurrencyConverter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class CurrencyConverter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Hello from the currency converter component!</h2>
+      </div>
+    );
+  }
+}
